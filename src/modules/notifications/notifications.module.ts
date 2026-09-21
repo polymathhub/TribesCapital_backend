d7a777db1a1d2446de05384daad531af771f1d7b
Tribes User <user@tribes.local>
2026-09-21 15:55:39 +0100

    Fix notification roles guard module dependency

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@database/database.module';
+import { RolesModule } from '@modules/roles/roles.module';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RolesModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService, DatabaseModule],
