6f8fd94d193a05528a3e77b98dd56b99378b4411
Tribes User <user@tribes.local>
2026-09-21 16:00:24 +0100

    Wire roles module into admin content modules

--- src/modules/courses/courses.module.ts
+++ src/modules/courses/courses.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@database/database.module';
+import { RolesModule } from '@modules/roles/roles.module';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RolesModule],
   controllers: [CoursesController],
   providers: [CoursesService],
   exports: [CoursesService],
